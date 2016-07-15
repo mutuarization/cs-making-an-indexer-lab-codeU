70c27c7de6e07333210f6df9841d26504be5f739
Eric Musyoka <ericmutua100@gmail.com>
2016-07-15 01:18:08 -0700

    Done.

--- javacs-lab06/src/com/flatironschool/javacs/TermCounter.java
+++ javacs-lab06/src/com/flatironschool/javacs/TermCounter.java
@@ -36,8 +36,11 @@ public class TermCounter {
 	 * @return
 	 */
 	public int size() {
-        // TODO: fill this in.
-		return -1;
+		int termCount = 0;
+		for(String key: map.keySet()) {
+			termCount += map.get(key);
+		}
+        return termCount;
 	}
 
 	/**
